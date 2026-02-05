489336716eea79739c71cc15e8381311a6ea0a00
Jarif Ahmed <your_github_email@gmail.com>
2026-02-05 15:08:46 +0600

    added all file codes

---
+++ spread.js
@@ -0,0 +1,35 @@
+// // const numbers = [87, 8, 5, 91]
+// // // console.log(numbers);
+// // // console.log(...numbers);
+
+// // // const max = Math.max(23, 45, 98, 56, 23);
+// // const max = Math.max(...numbers);
+// // // console.log(max);
+
+// // const first = [1, 2, 3, 5];
+// // const second = first;
+// // second.push(6);
+// // console.log(first)
+
+// const first = [1, 2, 3, 4, 5];
+// const second = [...first];
+// const third = [34, ...first, 56, 67];
+// second.push(6);
+// // console.log(first);
+// // console.log(second);
+// // console.log(third);
+
+// const ages = [45, 46, 1];
+// const prices = [76, 45, 34];
+// const all = [...ages, 333, ...prices];
+// // console.log(all);
+
+// const person = {name: 'Parmisan', age: 25};
+// const employee = {designation: 'dev', ...person};
+// // console.log(employee);
+
+// const total = (a, b, c) => a + b + c;
+// const result = total(45, 65, 21)
+// const digits = [78, 55, 33]
+// total(...digits);
+// console.log(digits);
